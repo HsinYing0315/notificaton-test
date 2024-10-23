0f63a7a382c0a79cfc11fa1a7edb44fe07559694
HsinYing0315 <wusinging0315@gmail.com>
2024-10-23 22:04:14 +0800

    chore: firebase setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: "",
+      firebaseAuthDomain: "",
+      firebaseProjectId: "",
+    }
+  },
+  nitro: {
+    firebase: {
+      gen: 2
+    }
+  }
 })
---
+++ plugins/firebase.client.ts
@@ -0,0 +1,31 @@
+import { defineNuxtPlugin } from "#app";
+import { type FirebaseApp, initializeApp } from "firebase/app";
+import { type Auth, getAuth } from "firebase/auth";
+import { type Functions, getFunctions } from "firebase/functions";
+import { type Messaging, getMessaging } from "firebase/messaging";
+
+declare module "#app" {
+  interface NuxtApp {
+    $firebaseApp: FirebaseApp;
+    $firebaseAuth: Auth;
+    $firebaseFunctions: Functions;
+    $firebaseMessaging: Messaging;
+  }
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig().public;
+  const firebaseConfig = {
+    apiKey: config.firebaseApiKey,
+    authDomain: config.firebaseAuthDomain,
+    projectId: config.firebaseProjectId,
+    storageBucket: "notification-test-88e92.appspot.com",
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  nuxtApp.provide("firebaseApp", firebaseApp);
+  nuxtApp.provide("firebaseAuth", getAuth(firebaseApp));
+  nuxtApp.provide("firebaseFunctions", getFunctions(firebaseApp));
+  nuxtApp.provide("firebaseMessaging", getMessaging(firebaseApp));
+});
