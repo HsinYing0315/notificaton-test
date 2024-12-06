2c74eb47334ef1100e36672dd2572e0a0a58dc64
HsinYing0315 <wusinging0315@gmail.com>
2024-12-06 10:45:16 +0800

    chore: pwa + https settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig, type NuxtConfig } from "nuxt/config"
+import path from "path"
+import fs from "fs"
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  buildModules: ['@nuxtjs/pwa'],
+  pwa: {
+    icon: false,
+    manifest: {
+      name: 'Nuxt Firebase Test',
+      startUrl: '/',
+      display: 'standalone',
+      useWebmanifestExtension: true,
+    },
+    workbox: {
+      autoRegister: true,
+      importScripts: ['~/assets/sw.js']
+    },
+    devOptions: {
+      enabled: true,
+    }
+  },
   runtimeConfig: {
     public: {
       firebaseApiKey: "",
       firebaseAuthDomain: "",
       firebaseProjectId: "",
+      firebaseMessagingSenderId: "",
+      firebaseAppId: "",
     }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+    },
   },
   nitro: {
     firebase: {
       gen: 2
     }
   }
-})
+} as NuxtConfig)
